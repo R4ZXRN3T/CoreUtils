fb1a31637961a3e1477f11384d422272d1b27bea
Arian Pieroni <rizelloyd@gmail.com>
2025-09-20 15:15:29 +0200

    Added a simple test

--- src/test/java/SortTests.java
+++ src/test/java/SortTests.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import org.R4ZXRN3T.ArrayUtils;
+import org.junit.jupiter.api.Test;
 
 public class SortTests {
+	@Test
+	void testSort() {
+		int[] input = {5, 2, 9, 1, 5, 6};
+		int[] expected = {1, 2, 5, 5, 6, 9};
+		int[] result = ArrayUtils.sort(input);
+		assertArrayEquals(expected, result);
+	}
 }
