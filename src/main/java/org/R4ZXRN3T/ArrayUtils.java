6641f76174efe5642eaa9a7f3583e80bdaa0af9c
Arian Pieroni <rizelloyd@gmail.com>
2025-09-20 15:15:14 +0200

    Now has a sort method for int arrays

--- src/main/java/org/R4ZXRN3T/ArrayUtils.java
+++ src/main/java/org/R4ZXRN3T/ArrayUtils.java
@@ -1,4 +1,9 @@
 package org.R4ZXRN3T;
 
 public class ArrayUtils {
+	public static int[] sort(int[] inputArray) {
+		int[] finalArray = inputArray.clone();
+		MergeSorts.mergeSort(finalArray, 0, inputArray.length - 1);
+		return finalArray;
+	}
 }
