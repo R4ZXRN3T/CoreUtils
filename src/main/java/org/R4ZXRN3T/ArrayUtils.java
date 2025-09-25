56f8c2c82d4e187f0c983e64540b0a86b0cfb11b
Arian Pieroni <rizelloyd@gmail.com>
2025-09-25 14:50:21 +0200

    Added methods for all primitive data types and one general for every object that implements a comparable

--- src/main/java/org/R4ZXRN3T/ArrayUtils.java
+++ src/main/java/org/R4ZXRN3T/ArrayUtils.java
@@ -2,8 +2,32 @@ package org.R4ZXRN3T;
 
 public class ArrayUtils {
 	public static int[] sort(int[] inputArray) {
-		int[] finalArray = inputArray.clone();
-		MergeSorts.mergeSort(finalArray, 0, inputArray.length - 1);
-		return finalArray;
+		MergeSorts.mergeSort(inputArray, 0, inputArray.length - 1);
+		return inputArray;
+	}
+
+	public static long[] sort(long[] inputArray) {
+		MergeSorts.mergeSort(inputArray, 0, inputArray.length - 1);
+		return inputArray;
+	}
+
+	public static float[] sort(float[] inputArray) {
+		MergeSorts.mergeSort(inputArray, 0, inputArray.length - 1);
+		return inputArray;
+	}
+
+	public static double[] sort(double[] inputArray) {
+		MergeSorts.mergeSort(inputArray, 0, inputArray.length - 1);
+		return inputArray;
+	}
+
+	public static short[] sort(short[] inputArray) {
+		MergeSorts.mergeSort(inputArray, 0, inputArray.length - 1);
+		return inputArray;
+	}
+
+	public static byte[] sort(byte[] inputArray) {
+		MergeSorts.mergeSort(inputArray, 0, inputArray.length - 1);
+		return inputArray;
 	}
 }
--- src/main/java/org/R4ZXRN3T/MergeSorts.java
+++ src/main/java/org/R4ZXRN3T/MergeSorts.java
@@ -1,40 +1,194 @@
 package org.R4ZXRN3T;
 
+import java.util.Arrays;
+
 class MergeSorts {
-	protected static void mergeSort(int[] inputArray, int leftIndex, int rightIndex) {
-		if (leftIndex < rightIndex) {
-			int middleIndex = leftIndex + (rightIndex - leftIndex) / 2;
-
-			mergeSort(inputArray, leftIndex, middleIndex);
-			mergeSort(inputArray, middleIndex + 1, rightIndex);
-
-			merge(inputArray, leftIndex, middleIndex, rightIndex);
+	protected static <T extends Comparable<T>> void mergeSort(T[] array, int left, int right) {
+		if (left < right) {
+			int mid = left + (right - left) / 2;
+			mergeSort(array, left, mid);
+			mergeSort(array, mid + 1, right);
+			merge(array, left, mid, right);
 		}
 	}
 
-	private static void merge(int[] inputArray, int leftIndex, int middleIndex, int rightIndex) {
-		int leftArrayLength = middleIndex - leftIndex + 1;
-		int rightArrayLength = rightIndex - middleIndex;
+	private static <T extends Comparable<T>> void merge(T[] array, int left, int mid, int right) {
+		int n1 = mid - left + 1;
+		int n2 = right - mid;
+		T[] leftArr = Arrays.copyOfRange(array, left, mid + 1);
+		T[] rightArr = Arrays.copyOfRange(array, mid + 1, right + 1);
 
-		int[] leftArray = new int[leftArrayLength];
-		int[] rightArray = new int[rightArrayLength];
-
-		System.arraycopy(inputArray, leftIndex, leftArray, 0, leftArrayLength);
-		System.arraycopy(inputArray, middleIndex + 1, rightArray, 0, rightArrayLength);
-
-		int i = 0, j = 0, k = leftIndex;
-
-		while (i < leftArrayLength && j < rightArrayLength) {
-			if (leftArray[i] <= rightArray[j]) {
-				inputArray[k++] = leftArray[i++];
+		int i = 0, j = 0, k = left;
+		while (i < n1 && j < n2) {
+			if (leftArr[i].compareTo(rightArr[j]) <= 0) {
+				array[k++] = leftArr[i++];
 			} else {
-				inputArray[k++] = rightArray[j++];
+				array[k++] = rightArr[j++];
 			}
 		}
-
-		while (i < leftArrayLength) inputArray[k++] = leftArray[i++];
-		while (j < rightArrayLength) inputArray[k++] = rightArray[j++];
+		while (i < n1) array[k++] = leftArr[i++];
+		while (j < n2) array[k++] = rightArr[j++];
 	}
 
-	// TODO: Add merge sort for other data types
+	protected static void mergeSort(int[] array, int left, int right) {
+		if (left < right) {
+			int mid = left + (right - left) / 2;
+			mergeSort(array, left, mid);
+			mergeSort(array, mid + 1, right);
+			merge(array, left, mid, right);
+		}
+	}
+
+	private static void merge(int[] array, int left, int mid, int right) {
+		int n1 = mid - left + 1;
+		int n2 = right - mid;
+		int[] leftArr = Arrays.copyOfRange(array, left, mid + 1);
+		int[] rightArr = Arrays.copyOfRange(array, mid + 1, right + 1);
+
+		int i = 0, j = 0, k = left;
+		while (i < n1 && j < n2) {
+			if (leftArr[i] <= rightArr[j]) {
+				array[k++] = leftArr[i++];
+			} else {
+				array[k++] = rightArr[j++];
+			}
+		}
+		while (i < n1) array[k++] = leftArr[i++];
+		while (j < n2) array[k++] = rightArr[j++];
+	}
+
+	protected static void mergeSort(long[] array, int left, int right) {
+		if (left < right) {
+			int mid = left + (right - left) / 2;
+			mergeSort(array, left, mid);
+			mergeSort(array, mid + 1, right);
+			merge(array, left, mid, right);
+		}
+	}
+
+	private static void merge(long[] array, int left, int mid, int right) {
+		int n1 = mid - left + 1;
+		int n2 = right - mid;
+		long[] leftArr = Arrays.copyOfRange(array, left, mid + 1);
+		long[] rightArr = Arrays.copyOfRange(array, mid + 1, right + 1);
+
+		int i = 0, j = 0, k = left;
+		while (i < n1 && j < n2) {
+			if (leftArr[i] <= rightArr[j]) {
+				array[k++] = leftArr[i++];
+			} else {
+				array[k++] = rightArr[j++];
+			}
+		}
+		while (i < n1) array[k++] = leftArr[i++];
+		while (j < n2) array[k++] = rightArr[j++];
+	}
+
+	protected static void mergeSort(float[] array, int left, int right) {
+		if (left < right) {
+			int mid = left + (right - left) / 2;
+			mergeSort(array, left, mid);
+			mergeSort(array, mid + 1, right);
+			merge(array, left, mid, right);
+		}
+	}
+
+	private static void merge(float[] array, int left, int mid, int right) {
+		int n1 = mid - left + 1;
+		int n2 = right - mid;
+		float[] leftArr = Arrays.copyOfRange(array, left, mid + 1);
+		float[] rightArr = Arrays.copyOfRange(array, mid + 1, right + 1);
+
+		int i = 0, j = 0, k = left;
+		while (i < n1 && j < n2) {
+			if (leftArr[i] <= rightArr[j]) {
+				array[k++] = leftArr[i++];
+			} else {
+				array[k++] = rightArr[j++];
+			}
+		}
+		while (i < n1) array[k++] = leftArr[i++];
+		while (j < n2) array[k++] = rightArr[j++];
+	}
+
+	protected static void mergeSort(double[] array, int left, int right) {
+		if (left < right) {
+			int mid = left + (right - left) / 2;
+			mergeSort(array, left, mid);
+			mergeSort(array, mid + 1, right);
+			merge(array, left, mid, right);
+		}
+	}
+
+	private static void merge(double[] array, int left, int mid, int right) {
+		int n1 = mid - left + 1;
+		int n2 = right - mid;
+		double[] leftArr = Arrays.copyOfRange(array, left, mid + 1);
+		double[] rightArr = Arrays.copyOfRange(array, mid + 1, right + 1);
+
+		int i = 0, j = 0, k = left;
+		while (i < n1 && j < n2) {
+			if (leftArr[i] <= rightArr[j]) {
+				array[k++] = leftArr[i++];
+			} else {
+				array[k++] = rightArr[j++];
+			}
+		}
+		while (i < n1) array[k++] = leftArr[i++];
+		while (j < n2) array[k++] = rightArr[j++];
+	}
+
+	protected static void mergeSort(short[] array, int left, int right) {
+		if (left < right) {
+			int mid = left + (right - left) / 2;
+			mergeSort(array, left, mid);
+			mergeSort(array, mid + 1, right);
+			merge(array, left, mid, right);
+		}
+	}
+
+	private static void merge(short[] array, int left, int mid, int right) {
+		int n1 = mid - left + 1;
+		int n2 = right - mid;
+		short[] leftArr = Arrays.copyOfRange(array, left, mid + 1);
+		short[] rightArr = Arrays.copyOfRange(array, mid + 1, right + 1);
+
+		int i = 0, j = 0, k = left;
+		while (i < n1 && j < n2) {
+			if (leftArr[i] <= rightArr[j]) {
+				array[k++] = leftArr[i++];
+			} else {
+				array[k++] = rightArr[j++];
+			}
+		}
+		while (i < n1) array[k++] = leftArr[i++];
+		while (j < n2) array[k++] = rightArr[j++];
+	}
+
+	protected static void mergeSort(byte[] array, int left, int right) {
+		if (left < right) {
+			int mid = left + (right - left) / 2;
+			mergeSort(array, left, mid);
+			mergeSort(array, mid + 1, right);
+			merge(array, left, mid, right);
+		}
+	}
+
+	private static void merge(byte[] array, int left, int mid, int right) {
+		int n1 = mid - left + 1;
+		int n2 = right - mid;
+		byte[] leftArr = Arrays.copyOfRange(array, left, mid + 1);
+		byte[] rightArr = Arrays.copyOfRange(array, mid + 1, right + 1);
+
+		int i = 0, j = 0, k = left;
+		while (i < n1 && j < n2) {
+			if (leftArr[i] <= rightArr[j]) {
+				array[k++] = leftArr[i++];
+			} else {
+				array[k++] = rightArr[j++];
+			}
+		}
+		while (i < n1) array[k++] = leftArr[i++];
+		while (j < n2) array[k++] = rightArr[j++];
+	}
 }
